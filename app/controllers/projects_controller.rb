4c88b5a1af476e94d18f676e4f2720fb90f0ff7c
Joonho Kim <lippoint.surf0622@gmail.com>
2026-06-07 11:41:03 +0900

    Initial commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,28 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+end
